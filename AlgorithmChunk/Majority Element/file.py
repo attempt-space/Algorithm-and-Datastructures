3139544c4b7401801acd2aa7546cfcd99c90e598
attempt-space <sivaprasath07121994@gmail.com>
2020-05-18 17:26:09 +0530

    :v: Majority Element

---
+++ AlgorithmChunk/Majority Element/file.py
@@ -0,0 +1,26 @@
+"""
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2
+"""
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        from collections import Counter
+        c  = Counter(nums)
+        length = len(nums)
+        """
+        for key,value in c.items():
+            if value > length//2:
+                return key
+        """
+        return c.most_common()[0][0]
