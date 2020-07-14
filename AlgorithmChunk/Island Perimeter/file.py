a084a0ff6020d4ba97d1a7afbacdde1eb80aae69
attempt-space <sivaprasath07121994@gmail.com>
2020-07-14 17:28:13 +0530

    Island Perimeter

---
+++ AlgorithmChunk/Island Perimeter/file.py
@@ -0,0 +1,61 @@
+"""
+You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water.
+
+Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+
+The island doesn't have "lakes" (water inside that isn't connected to the water around the island). One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+
+ 
+
+Example:
+
+Input:
+[[0,1,0,0],
+ [1,1,1,0],
+ [0,1,0,0],
+ [1,1,0,0]]
+
+Output: 16
+
+Explanation: The perimeter is the 16 yellow stripes in the image below:
+"""
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        """
+        row = len(grid)
+        column = len(grid[0])
+        perimeter = 0
+        
+        for i in range(row):
+            for j in range(column):
+                if grid[i][j]:
+                    count = 0
+                    if (i > 0  and grid[i-1][j]):                        
+                        count+=1
+                    if (j > 0 and grid[i][j-1]):
+                        count+=1
+                    if (i < row-1 and grid[i+1][j]):                        
+                        count+=1
+                    if (j < column-1 and grid[i][j+1]):                                
+                        count+=1
+                        
+                    perimeter += (4 - count)
+                
+                
+        return perimeter
+        """
+        m, n = len(grid), len(grid[0])
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] ==1:
+                    res +=4
+                    if i>0 and grid[i-1][j]:
+                        res -=2
+                    if j>0 and grid[i][j-1]:
+                        res -=2
+        return res
+                
+                
+        
+        
